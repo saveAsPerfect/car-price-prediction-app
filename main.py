4e30d3a9057de10698fae2dc94ed6479c3ad0b09
saveAsPerfect <133439601+saveAsPerfect@users.noreply.github.com>
2024-07-03 07:33:11 +0600

    added model

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import streamlit as st
-
+import pickle as pk
 
 def main():
-    pass
+    with open('model.pkl','rb') as f:
+        model = pk.load(f)
+    
+    st.set_page_config(
+        layout="centered",
+        page_title="Car Price",
+        initial_sidebar_state="auto"
+    )
+
+    st.write(
+        """
+        # Car Price Prediction
+
+        """
+    )
+    side_bar()
+
+
+def side_bar():
+    st.sidebar.header('inputs')
 
 
 if __name__ == "__main__":
